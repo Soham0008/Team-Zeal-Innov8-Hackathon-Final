4f6d489c38d13763a69780f6e252d14c5de8650a
Soham0008 <patilsoham0008@gmail.com>
2024-09-29 09:38:14 +0530

    rank added

--- Source Code/convert_json.py
+++ Source Code/convert_json.py
@@ -11,10 +11,13 @@ df.rename(columns={'Unnamed: 0': 'candidate_id'}, inplace=True)
 selected_columns = ['candidate_id', 'name', 'score_5']
 df_selected = df[selected_columns]
 
-# Step 4: Convert the DataFrame to a JSON string
+# Step 4: Add a 'rank' column in increasing order
+df_selected['rank'] = range(1, len(df_selected) + 1)
+
+# Step 5: Convert the DataFrame to a JSON string
 json_str = df_selected.to_json(orient='records')
 
-# Step 5: Save the JSON string to a file
+# Step 6: Save the JSON string to a file
 json_file_path = 'Datasets/final.json'
 with open(json_file_path, 'w') as json_file:
     json_file.write(json_str)
