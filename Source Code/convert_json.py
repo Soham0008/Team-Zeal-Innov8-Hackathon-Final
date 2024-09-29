0f7d6b5f8d8caaaa4552f06c8fa3446fd94f11a1
Soham0008 <patilsoham0008@gmail.com>
2024-09-29 09:16:58 +0530

    conv2Json

--- Source Code/convert_json.py
+++ Source Code/convert_json.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 # Step 1: Read the CSV file
-csv_file_path = 'Datasets/rankings_rec.csv'
+csv_file_path = 'Datasets/final_dataset_ranked_ordered (1).csv'
 df = pd.read_csv(csv_file_path)
 
 # Step 2: Select specific columns
-selected_columns = ['candidate id', 'position', 'recommenders', 'secore_5']
+selected_columns = ['Unnamed: 0', 'name', 'score_5']
 df_selected = df[selected_columns]
 
 # Step 3: Convert the DataFrame to a JSON string
 json_str = df_selected.to_json(orient='records')
 
 # Step 4: Save the JSON string to a file
-json_file_path = 'rankings_rec.json'
+json_file_path = 'Datasets/final.json'
 with open(json_file_path, 'w') as json_file:
     json_file.write(json_str)
